8f6da0d4ec7910727bf6a35eb527820a0b0c7bd8
thetime2009 <thetime.pota@gmail.com>
2026-05-08 22:13:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const BLYNK_TOKEN = "r6cAEnogc2zRH2BkAr7TTESFcya1osDf";
-// const BLYNK_URL = "http://blynk.iot-cm.com:8080/"; 
-const BLYNK_URL = "https://api.allorigins.win/raw?url=http://blynk.iot-cm.com:8080/";
+const BLYNK_URL = "http://blynk.iot-cm.com:8080/"; 
 
 // =====================
 // ⏰ TIME FUNCTIONS
