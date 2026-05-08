a2a549309bd1f6b518ff7da51aa508b27f2aaa1f
thetime2009 <thetime.pota@gmail.com>
2026-05-08 13:13:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const BLYNK_URL = "http://blynk.iot-cm.com:8080/";
 async function fetchData() {
     try {
         // ดึงค่าเซนเซอร์ และสถานะไฟ LED (V21-V24)
-        const pins = ['V10' , 'V1', 'V0', 'V65', 'V18', 'V105', 'V11','V12','V13','V14','V21', 'V22', 'V23', 'V24'];
+        const pins = ['V10' , 'V1', 'V0', 'V65', 'V18', 'V105','V106', 'V11','V12','V13','V14','V21', 'V22', 'V23', 'V24'];
         
         for (let pin of pins) {
             const response = await fetch(`${BLYNK_URL}${BLYNK_TOKEN}/get/${pin}`);
@@ -29,6 +29,7 @@ function updateUI(pin, value) {
     if (pin === 'V65') document.getElementById('rain').innerText = cleanValue;
     if (pin === 'V18') document.getElementById('water_used').innerText = cleanValue;
     if (pin === 'V105') document.getElementById('vpd_val').innerText = cleanValue;
+    if (pin === 'V106') document.getElementById('status_val').innerText = cleanValue;
 
     // --- ส่วนอัปเดตปุ่ม Switch ระบบอัตโนมัติ (V10) ---
     if (pin === 'V10') {
