0677e730c25e1df32638df96a449418b3b27b85e
thetime2009 <thetime.pota@gmail.com>
2026-05-09 15:20:50 +0700

    Change BLYNK_URL to use HTTP instead of HTTPS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // 🔑 CONFIG & VARIABLES
 // =====================
 const BLYNK_TOKEN = "r6cAEnogc2zRH2BkAr7TTESFcya1osDf";
-const BLYNK_URL = "https://blynk.iot-cm.com:8080/"; 
+const BLYNK_URL = "http://blynk.iot-cm.com:8080/"; 
 let farmChart; // ตัวแปรสำหรับคุมกราฟ
 
 // =====================
